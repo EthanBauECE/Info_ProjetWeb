5b9cf470f9386ffc26534648f6be9b772a6c62d8
alicemai <alice.coste@edu.ece.fr>
2025-05-28 10:56:54 +0200

    update

--- parcourir.php
+++ parcourir.php
@@ -14,20 +14,20 @@
                 <h3>1. Médecine générale</h3>
                 <p>Consultez la liste complète de nos médecins généralistes, leur disponibilité, leur CV, et prenez
                     rendez-vous directement.</p>
-                <a href="medecine_general.html">Explorer Médecine générale</a>
+                <a href="medecine_general.php">Explorer Médecine générale</a>
             </div>
 
             <div class="info-card">
                 <h3>2. Médecins spécialistes</h3>
                 <p>Découvrez nos spécialistes dans plusieurs domaines : cardiologie, dermatologie, pédiatrie, etc.</p>
-                <a href="medecins_special.html">Explorer Médecins spécialistes</a>
+                <a href="medecins_special.php">Explorer Médecins spécialistes</a>
             </div>
 
             <div class="info-card">
                 <h3>3. Laboratoire de biologie médicale</h3>
                 <p>Accès aux services de prélèvement, analyses biologiques, bilans complets avec suivi de vos résultats.
                 </p>
-                <a href="#">Explorer Laboratoire</a>
+                <a href="labo.php">Explorer Laboratoire</a>
             </div>
         </section>
     </main>
